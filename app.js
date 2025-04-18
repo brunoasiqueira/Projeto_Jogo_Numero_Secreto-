5f077e64fb0a6214be281ec550a06a2280282613
BRUNO ALVES DE SIQUEIRA <brunoalves.siqueira@gmail.com>
2025-04-18 16:28:38 -0300

    Finalização do código (fim do projeto)

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+let listaDeNumeroSorteado = [];
+let numeroLimite = 10;
 let NumeroSecreto = gerarNumeroAleatorio ();
 let tentativas = 1;
 
 function exibirTextoNaTela (tag, texto) {
     let campo = document.querySelector (tag);
     campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 function exibirMensagemInicial () {
     exibirTextoNaTela ("h1", "Jogo do Número Secreto");
-    exibirTextoNaTela ("p", "Tente adivinhar o número secreto entre 1 e 10!");
+    exibirTextoNaTela ("p", "Escolha um número entre 1 e 10");
 }
 
 exibirMensagemInicial ();
@@ -24,9 +34,9 @@ function verificarChute () {
         document.getElementById ("reiniciar").removeAttribute ("disabled");
     } else { 
         if (chute > NumeroSecreto) {
-        exibirTextoNaTela ("p", "O número secreto é menor que o chute");
+        exibirTextoNaTela ("p", "O número secreto é menor");
         } else {
-        exibirTextoNaTela ("p", "O número secreto é maior que o chute");
+        exibirTextoNaTela ("p", "O número secreto é maior");
         }
         tentativas++;
         limparCampo ();
@@ -34,7 +44,19 @@ function verificarChute () {
 }
 
 function gerarNumeroAleatorio () {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumeroSorteado.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumeroSorteado = [];
+    }
+    if (listaDeNumeroSorteado.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio ();
+    } else {
+        listaDeNumeroSorteado.push (numeroEscolhido);
+        console.log (listaDeNumeroSorteado);
+        return numeroEscolhido;
+    }
 } 
 
 function limparCampo () {
